1446dd613ec618ce9d93c8cf04460f97843cca08
BenjaminEtchegaray <ben.etchegaray@gmail.com>
2022-11-28 12:11:18 +0100

    add incons on contact and style changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,12 @@
 import './style.scss';
+import { GitHub, Linkedin } from 'react-feather';
 
 const Contact = () => (
   <div className="contact">
     <ul className="contact_list">
-      <a className="contact_item_git" href="https://github.com/BenjaminEtchegaray">Git</a>
-      <a className="contact_item_email" href="#">Email</a>
-      <a className="contact_item_linkedin" href="https://www.linkedin.com/in/benjamin-etchegaray/">Linkedin</a>
+      <a className="contact_list_git" href="https://github.com/BenjaminEtchegaray">Github <GitHub className="contact_list_item_git_logo" /></a>
+      <a className="contact_list_email" href="mailto:ben.etchegaray@gmail.com">Email</a>
+      <a className="contact_list_linkedin" href="https://www.linkedin.com/in/benjamin-etchegaray/">Linkedin <Linkedin className="contact_list_item_linkedin_logo" /></a>
     </ul>
   </div>
 );

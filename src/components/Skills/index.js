d0c9693dc436cf1cc13421b5f0a7941d8aac92b3
BenjaminEtchegaray <ben.etchegaray@gmail.com>
2022-12-01 14:43:51 +0100

    progress bar added for skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,23 +1,27 @@
 import './style.scss';
+import { Line } from 'rc-progress';
 import { skillsList } from '../../data/skills';
 
 const Skills = () => (
   <div className="skills">
-    <ul className="skills_list">
-
-      <h1 className="skills_list_title">Languages :</h1>
-      {skillsList[0].languages.map((language) => (
-        <li className="skills_list_item" key={language.id}>{language.name} {language.icon }</li>
-      ))}
-    </ul>
-    <ul className="skills_list">
-      <h1 className="skills_list_title">Frameworks :</h1>
-      {skillsList[1].frameworks.map((framework) => (
-        <li className="skills_list_item" key={framework.id}>{framework.name} {framework.icon }</li>
-      ))}
-    </ul>
+    <section className="skills_list">
+      <ul className="skills_list_languages">
+        <h1 className="skills_list_languages_title">Languages :</h1>
+        {skillsList[0].languages.map((language) => (
+          <li className="skills_list_languages_item" key={language.id}>{language.name} {language.icon } <Line percent={language.progress} strokeWidth={1} strokeColor={language.color} /></li>
+        ))}
+      </ul>
+    </section>
+    <section className="skills_list">
+      <ul className="skills_list_frameworks">
+        <h1 className="skills_list_frameworks_title">Frameworks :</h1>
+        {skillsList[1].frameworks.map((framework) => (
+          <li className="skills_list_frameworks_item" key={framework.id}>{framework.name} {framework.icon } <Line percent={framework.progress} strokeWidth={1} strokeColor={framework.color} /></li>
+        ))}
+      </ul>
+    </section>
   </div>
 );
-console.log(skillsList);
+// console.log(skillsList);
 
 export default Skills;
--- src/data/skills.js
+++ src/data/skills.js
@@ -10,6 +10,8 @@ export const skillsList = [
         id: 1,
         name: 'HTML',
         icon: <img src="https://img.icons8.com/fluency/48/null/html-5.png" alt="html icon" />,
+        progress: 75,
+        color: '#ff9633',
 
       },
 
@@ -17,18 +19,24 @@ export const skillsList = [
         id: 2,
         name: 'CSS',
         icon: <img src="https://img.icons8.com/color/48/null/css3.png" alt="css icon" />,
+        progress: 65,
+        color: '#399CFF',
       },
 
       {
         id: 3,
         name: 'JAVASCRIPT',
         icon: <img src="https://img.icons8.com/color/48/null/javascript--v1.png" alt="javascript icon" />,
+        progress: 70,
+        color: '#FFC631',
       },
 
       {
         id: 4,
         name: 'PHP',
         icon: <img src="https://img.icons8.com/officel/40/null/php-logo.png" alt="php icon" />,
+        progress: 40,
+        color: '#8858F6',
       },
     ],
   },
@@ -42,7 +50,8 @@ export const skillsList = [
         id: 1,
         name: 'REACT.JS',
         icon: <img src="https://img.icons8.com/office/40/null/react.png" alt="react icon" />,
-
+        progress: 60,
+        color: '#87CEFA',
       },
     ],
   },
